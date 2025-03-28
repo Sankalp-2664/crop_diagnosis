923d756608ec7fb824a669968e375c5bc4280987
Sankalp Chakre <121400593+Sankalp-2664@users.noreply.github.com>
2025-03-28 09:24:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,4 +123,5 @@ def predict():
     return jsonify({"diseases": result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
